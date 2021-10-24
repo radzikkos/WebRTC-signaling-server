4050b2a2d27bbfc779599c3d045915e80c7260fe
radzikkos <lukaszrad99@wp.pl>
2021-10-24 21:11:28 +0200

    calling to chosen peer added

--- server.js
+++ server.js
@@ -6,13 +6,43 @@ const server = http.createServer((req, res) => {
   res.end("I am connected");
 });
 const wss = new websocket.Server({ server });
-// const { v4: uuidv4 } = require("uuid");
+
+function deleteFromConnectedPeers(array, client) {
+  const index = array.findIndex((el) => {
+    return el.id == client.id;
+  });
+  if (index != -1) {
+    array.splice(index, 1);
+    return true;
+  }
+  return false;
+}
+function addToConnectedPeers(ws) {
+  connectedPeers.push({ client: ws, id: ws.id });
+}
+
+function broadcastConnectedPeers(wss, ws) {
+  const object = {
+    peers: connectedPeers.map((peer) => {
+      return { id: peer.id };
+    }),
+    type: "peers",
+  };
+  wss.clients.forEach((client) => {
+    if (client.readyState === websocket.OPEN) {
+      client.send(JSON.stringify(object));
+    }
+  });
+}
 let peersNo = 0;
+const connectedPeers = [];
 wss.on("connection", (ws, req) => {
   console.log("Peer connected");
   peersNo++;
   ws.id = peersNo;
+  addToConnectedPeers(ws);
   ws.send(JSON.stringify({ type: "myId", id: ws.id }));
+  broadcastConnectedPeers(wss);
   //receive the message from client on Event: 'message'
   ws.on("message", (msg) => {
     // console.log("sending message");
@@ -35,14 +65,28 @@ wss.on("connection", (ws, req) => {
             console.log("Sending sending new candidate from ", object.peerType);
             client.send(JSON.stringify(object));
           } else if (msg && object.type === "initiating") {
-            console.log("Initiate message");
-            client.send(JSON.stringify(object));
+            if (object.toId) {
+              console.log("SEND TO ONE CLIENT");
+              if (client.id == object.toId) client.send(JSON.stringify(object));
+            } else {
+              console.log("Initiate message");
+              client.send(JSON.stringify(object));
+            }
           }
         } catch (err) {
           console.log(err);
         }
       }
     });
+  });
+  ws.on("close", () => {
+    deleteFromConnectedPeers(connectedPeers, ws);
+    console.log(
+      connectedPeers.map((el) => {
+        return el.id;
+      })
+    );
+    broadcastConnectedPeers(wss);
   });
 });
 
